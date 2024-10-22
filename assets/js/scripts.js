80eac874b133e8e490f312ef4ad8b66f9a9b5a7b
bayesianvictor <bayesianvictor@gmail.com>
2024-10-22 21:36:25 +0100

    asd

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,8 @@
-// Custom scripts can be added here.
+// Automatic year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Load the navbar from nav.html into the placeholder
+function loadNavbar() {
+    document.getElementById('navbar-placeholder').innerHTML = "<nav><p>Navbar Loaded!</p></nav>";
+}
+window.onload = loadNavbar;
