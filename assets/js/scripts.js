be45a9ea7e3b7ce68b59476084ccd531c582e05b
bayesianvictor <bayesianvictor@gmail.com>
2024-10-22 21:38:47 +0100

    asd

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,18 @@ document.getElementById('currentYear').textContent = new Date().getFullYear();
 
 // Load the navbar from nav.html into the placeholder
 function loadNavbar() {
-    document.getElementById('navbar-placeholder').innerHTML = "<nav><p>Navbar Loaded!</p></nav>";
+    console.log('Trying to load navbar...');
+    fetch('nav.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log('Navbar loaded successfully');
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.log('Error loading the navbar:', error));
 }
 window.onload = loadNavbar;
